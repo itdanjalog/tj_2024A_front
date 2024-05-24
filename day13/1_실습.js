68754c26850ab08348844ed59f1056df4dbf90c5
itdanja <itdanja@kakao.com>
2024-05-24 13:21:34 +0900

    day13

--- day13/1_실습.js
+++ day13/1_실습.js
@@ -116,7 +116,7 @@ function _allRead(){
   // 3. 출력/대입 
   tableBody.innerHTML = html;
 }
-// 3. 개별출력 실행조건 : 1.제목 클릭했을때
+// 3. 개별출력 실행조건 : (누구를) 1.제목 클릭했을때
 function _read( index ){
   // 선택/클릭 했을때 클릭된 해당 인덱스를 매개변수를 받아서 
   let boardArray = boardList[ index ].split(','); // 해당 인덱스의 게시물 정보를 ,쉼표 기준으로 분류된 배열 반환 
@@ -130,12 +130,54 @@ function _read( index ){
                 <span> 작성일 : ${ boardArray[3] } </span>
               </div>
               <div> ${ boardArray[1] } </div>
-              <button>수정</button>
-              <button>삭제</button>`;
+              <button onclick="_update( ${ index } )">수정</button>
+              <button onclick="_delete( ${ index } )">삭제</button>`;
   // 3. 출력/대입 
   viewPage.innerHTML = html;
 }
-// 4.
-function _update(){ }
-// 5.
-function _delete(){ }
+// 4. 수정함수 실행조건 : (누구를)1. 수정버튼 클릭할떄  
+function _update( index ){ 
+  // 패스워드 검증 함수를 호출후 반환값이 false 이면 수정함수 강제종료 
+  if( _pwConfirm(index) == false ) return;
+
+  // 배열내 특정 데이터 수정 
+  let board = boardList[index].split(',');   // 1. 해당 게시물 분류 
+    // 새로운 제목과 내용 입력받는다.
+  let utitle = prompt('수정할제목');
+  let ucontent = prompt('수정할내용');
+    // 수정할 정보로 구성 : 새로운 제목과 내용만 수정 변수로 구성 하고 나머지는 기존 데이터 사용
+  let uboard = `${ utitle },${ ucontent },${ board[2]},${ board[3]},${ board[4]}`;
+    // 선택된 인덱스의 수정할 정보 대입 
+  boardList[index] = uboard;
+    // 화면 새로고침( 재 출력: 데이터 변화가 있기 때문에  )
+  _allRead();
+  _read( index );
+} // for end 
+
+// 5. 삭제함수 실행조건 : (누구를)1. 삭제버튼 클릭할때 
+function _delete( index ){
+  // 패스워드 검증 함수를 호출후 반환값이 false 이면 삭제함수 강제종료 
+  if( _pwConfirm(index) == false ) return;
+
+  // 배열내 특정 인덱스 삭제 
+  boardList.splice( index , 1 );
+  // 화면 새로고침( 재 출력: 데이터 변화가 있기 때문에  )
+  _allRead();
+  document.querySelector('#viewPage').innerHTML = ``;
+
+}
+
+// 6. 패스워드 체크 함수 
+function _pwConfirm( index ){
+  // - 패스워드 검증 
+  // 1. 검증받을 패스워드 입력받는다.
+  let confirmPw = prompt('비밀번호:');
+  // 2. 해당 인덱스의 비밀번호 와 입력받은 비밀번호가 다르면 함수를 중간에 종료한다.
+  if( confirmPw != boardList[index].split(',')[2] ){
+    alert('패스워드가 다릅니다. ');
+    return false ; // 패스워드 일치 실패 뜻 , 함수 종료 , 이하 아래 코드는 실행이 안됨.
+  }
+  return true; // 패스워드 일치 성공 뜻 
+}
+
+// + 조회수 증가함수 생각해보기
