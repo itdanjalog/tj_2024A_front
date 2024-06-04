1704d740afdcd5769e84ace6c23798633a204dd3
itdanja <itdanja@kakao.com>
2024-06-04 11:18:41 +0900

    day20

---
+++ day19/board.js
@@ -0,0 +1,31 @@
+/*
+
+  전체 글 출력 페이지 
+    - 글 등록시 모든 게시물이 localStorage 저장된 상태
+*/
+
+let boardList = [];
+
+// 1. 출력함수 : js가 열렸을때
+boardPrint();
+function boardPrint(){
+  // * 
+  boardList = JSON.parse( localStorage.getItem( 'boardList' )) ;
+  if( boardList == null ){ boardList = [ ]; }
+  
+  // 1. 어디에
+  let boardTbody = document.querySelector('#boardTbody'); 
+  // 2. 무엇을
+  let html = '';
+  for( let i = 0 ; i < boardList.length ; i++  ){
+    html += `<tr> 
+              <th> ${ boardList[i].no }  </th> 
+              <th> ${ boardList[i].title }  </th> 
+              <th> ${ boardList[i].writer }  </th> 
+              <th> ${ boardList[i].date }  </th> 
+              <th> ${ boardList[i].view }  </th>  
+            </tr>`
+  }
+  // 3. 출력/대입 
+  boardTbody.innerHTML = html ; 
+}
