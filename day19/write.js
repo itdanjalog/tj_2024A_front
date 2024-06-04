b8fb1b6e1e8db85e9d840f4edacaec97262a07c5
itdanja <itdanja@kakao.com>
2024-06-04 11:07:56 +0900

    day20

--- day19/write.js
+++ day19/write.js
@@ -17,24 +17,58 @@
 
 */
 
+// ------ 로그인 상태 유효성검사 -------- //
+  // 1. 브라우저 세션에 저장된 로그인된 회원번호 
+let loginNo = sessionStorage.getItem('loginNo'); 
+  // 2. 만약에 없으면 로그인 페이지 이동 
+if( loginNo == null ){ 
+  alert('로그인후 글쓰기가 가능합니다'); 
+  location.href="login.html";
+}
+
 let boardList = []
 
 function _write(){ console.log( "wirte()");
+  // * 
+  boardList = JSON.parse( localStorage.getItem( 'boardList' ) ) ;
+  if( boardList == null ){ boardList = []; }
+
   // 1. HTML 입력된 데이터 가져오기 
   let title = document.querySelector('#title').value;     
   let content = document.querySelector('#content').value; 
   // 2. 유효성검사 
   // 3. 데이터 가공 ( 입력외 추가정보 , 원하는 데이터형식 => 객체)
+    // 작성자( 로그인된 회원 =  sessionStorage 에 회원번호 )
+  let writer = ``; // 로그인된 회원의 아이디를 찾아서 넣을 변수 준비 
+
+  let memberList = []; // localStorage 에 있는 회원목록 호출 
+  memberList = JSON.parse( localStorage.getItem('memberList') ) ;
+  if( memberList == null ){ memberList = []; }
+  // 회원목록에서 로그인된 회원번호와 같은 객체 찾기 
+  for( let i = 0 ; i < memberList.length ; i++ ){
+    if( memberList[i].no == loginNo ){
+      writer = memberList[i].id; // 찾은 객체의 아이디를 writer변수에 대입 
+      break;      // for문 탈출 
+      // return;  // 함수 탈출 // 현재 상황에서는 return 금지 
+    }
+  }
+    // 게시물 객체 
+  console.log( boardList );
   let board = {
-    no : 1 ,              // 게시물번호
+    no : boardList.length == 0 ? 1 : boardList[boardList.length-1].no +1 ,              // 게시물번호
     title : title ,       // 게시물제목
     content : content ,   // 게시물내용
-    writer : 'qweqwe' ,   // 게시물작성자
+    writer :  writer,   // 게시물작성자
     date : new Date() ,   // 게시물작성일
     view : 1              // 게시물조회수 
   };
+
   // 4. 저장 
   boardList.push( board ); 
+
+    // * 
+  localStorage.setItem( 'boardList' , JSON.stringify( boardList ) );
+
   // 5. 안내후 이후 처리 
   alert('글쓰기 성공'); 
   location.href="board.html"; // JS에서 페이지 전환할때 사용되는 속성 
